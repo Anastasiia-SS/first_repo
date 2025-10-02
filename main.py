a9cfa24060fcf0bc3f76a72f7fe267f520c73771
Anastasiia-SS <fire0bee@gmail.com>
2025-10-02 19:39:02 +0300

    third commit

--- main.py
+++ main.py
@@ -1,3 +1,35 @@
 print("Hello world")
 
 print("Hello git")
+
+age = 32
+print(age)
+
+name = "Anastasiia"
+print(name)
+
+age = "banana"
+print(age)
+
+rate = 1.68
+value = 326
+payment = rate * value
+print(payment)
+
+rate = 1.68
+rate_night = rate / 2
+value_day = 221
+value_night = 196
+payment = rate * value_day + rate_night * value_night
+print(payment)
+
+first_name = "Anastasiia"
+last_name = "Siryk"
+full_name = first_name + " " + last_name
+print(full_name)
+
+length = 2.75
+width = 1.75
+area = length * width
+show = f"With width {width}m and length {length}m of the area is {area}m2"
+print(show)
