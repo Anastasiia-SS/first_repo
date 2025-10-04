b5fe0211c7bc147cf56f1c102f21b8cfd44ee838
Anastasiia-SS <fire0bee@gmail.com>
2025-10-04 11:50:15 +0300

    Task 2

--- main1.py
+++ main1.py
@@ -1,1 +1,11 @@
 
+
+work_experience = int(input("Enter your full work experience in years: "))
+
+if 1 < work_experience <= 5:
+    developer_type = "Middle"
+elif work_experience <= 1:
+    developer_type = "Junior"
+else:
+    developer_type = "Senior"
+print(developer_type)
