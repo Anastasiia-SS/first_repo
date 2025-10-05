a2a0536f668f996a7072ac98bdf388decdcd22ba
Anastasiia-SS <fire0bee@gmail.com>
2025-10-05 21:01:11 +0300

    Task 7

--- main1.py
+++ main1.py
@@ -1,9 +1,3 @@
-pool = 1000
-quantity = int(input("Enter the number of mailings: "))
-try:
-    chunk = pool / quantity
-    chunk = int(chunk)
-    print(f'Each person gets {chunk} units of currency.')
-except ZeroDivisionError:
-    print('Divide by zero completed!')
-
+def greeting():
+	print('Hello world!')
+greeting()
