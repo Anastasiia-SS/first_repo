50f578024363f907ca19d5c7696630e56edd2ce6
Anastasiia-SS <fire0bee@gmail.com>
2025-10-05 21:32:49 +0300

    Task 8

--- main1.py
+++ main1.py
@@ -1,3 +1,7 @@
-def greeting():
-	print('Hello world!')
-greeting()
+def invite_to_event(username: str) -> str:
+    return f"Dear {username}, we have the honor to invite you to our event!"
+name = invite_to_event("Anastasiia")
+print(name)
+
+
+
---
+++ test.py
@@ -0,0 +1,5 @@
+def greet(name: str) -> str:
+    return f"Привіт, {name}!"
+
+greeting = greet("Олексій")
+print(greeting)  # Виведе: Привіт, Олексій!
