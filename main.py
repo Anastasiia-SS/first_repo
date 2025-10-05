274118e2eccf71b859c20c675f2628600fe77b75
Anastasiia-SS <fire0bee@gmail.com>
2025-10-05 23:00:57 +0300

    2_Task 11

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def get_fullname(first_name, last_name, middle_name=''):
-    if middle_name:
-        full_name = f"{first_name} {middle_name} {last_name}"
-    else:
-        full_name = f"{first_name} {last_name}"
-    return full_name.title()
+def format_string(string, length):
+    if len(string) >= length:
+        return string
+    spaces = (length - len(string)) // 2
+    return (' ' * spaces + string)
