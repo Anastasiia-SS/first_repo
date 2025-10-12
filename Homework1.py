37121214d8d7acdd13f67ece4c6e25955b7a0cae
Anastasiia-SS <fire0bee@gmail.com>
2025-10-12 14:19:29 +0300

    Task 4 3

--- Homework1.py
+++ Homework1.py
@@ -1,29 +1,18 @@
 import re
 
-phone_numbers = """    067\\t123 4567",
-    "(095) 234-5678\\n",
-    "+380 44 123 4567",
-    "380501234567",
-    "    +38(050)123-32-34",
-    "     0503451234",
-    "(050)8889900",
-    "38050-111-22-22",
-    "38050 111 22 11   """
-
 def normalize_phone(phone_number):
-    digits = re.sub(r'\D', '', phone_number)
+    clean_number = re.sub(r'[^0-9+]', '', phone_number)
 
-    if digits.startswith('380'):
-        normalized = '+' + digits
-    elif digits.startswith('0'):
-        normalized = '+38' + digits
-    elif digits.startswith('38'):
-        normalized = '+' + digits
-    else:
-        normalized = '+38' + digits
+    if clean_number.startswith('+380'):
+        return clean_number
 
-    return normalized
+    elif clean_number.startswith('380'):
+        return '+' + clean_number
 
-lines = phone_numbers.strip().splitlines()
-for line in lines:
-    print(normalize_phone(line))
+    elif not clean_number.startswith('+'):
+        return '+38' + clean_number
+
+    return clean_number
+
+print(normalize_phone(" 093tf6rxdsud756436 "))   
+
