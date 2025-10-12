801cc651290284d98ecfa70223ee3e0aacfca103
Anastasiia-SS <fire0bee@gmail.com>
2025-10-12 14:14:59 +0300

    Task 4 3

--- Homework1.py
+++ Homework1.py
@@ -1,7 +1,29 @@
-import random
+import re
 
-def get_numbers_ticket(min, max, quantity):
-    if min < 1 or max > 1000 or quantity > (max - min + 1) or min > max:
-        return []
-    return random.sample(range(min, max + 1), quantity)
-print(get_numbers_ticket(600, 1000, 6))
+phone_numbers = """    067\\t123 4567",
+    "(095) 234-5678\\n",
+    "+380 44 123 4567",
+    "380501234567",
+    "    +38(050)123-32-34",
+    "     0503451234",
+    "(050)8889900",
+    "38050-111-22-22",
+    "38050 111 22 11   """
+
+def normalize_phone(phone_number):
+    digits = re.sub(r'\D', '', phone_number)
+
+    if digits.startswith('380'):
+        normalized = '+' + digits
+    elif digits.startswith('0'):
+        normalized = '+38' + digits
+    elif digits.startswith('38'):
+        normalized = '+' + digits
+    else:
+        normalized = '+38' + digits
+
+    return normalized
+
+lines = phone_numbers.strip().splitlines()
+for line in lines:
+    print(normalize_phone(line))
--- test.py
+++ test.py
@@ -1,5 +1,30 @@
-import math
+import re
 
-r = math.isclose(0.1, 0.10000000008)
-print(r)  # Це поверне True
+phone_numbers = """    067\\t123 4567",
+    "(095) 234-5678\\n",
+    "+380 44 123 4567",
+    "380501234567",
+    "    +38(050)123-32-34",
+    "     0503451234",
+    "(050)8889900",
+    "38050-111-22-22",
+    "38050 111 22 11   """
+
+def normalize_phone(phone_number):
+    digits = re.sub(r'\D', '', phone_number)
+
+    if digits.startswith('380'):
+        normalized = '+' + digits
+    elif digits.startswith('0'):
+        normalized = '+38' + digits
+    elif digits.startswith('38'):
+        normalized = '+' + digits
+    else:
+        normalized = '+38' + digits
+
+    return normalized
+
+lines = phone_numbers.strip().splitlines()
+for line in lines:
+    print(normalize_phone(line))
 
