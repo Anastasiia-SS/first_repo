471a9eee554bda2f1951f0c5f33a770eb8b1198a
Anastasiia-SS <fire0bee@gmail.com>
2025-10-13 12:26:56 +0300

    Task 4 2

--- Homework4_2.py
+++ Homework4_2.py
@@ -3,5 +3,6 @@ import random
 def get_numbers_ticket(min, max, quantity):
     if min < 1 or max > 1000 or quantity > (max - min + 1) or min > max:
         return []
+    numbers = random.sample(range(min, max + 1), quantity)
     return random.sample(range(min, max + 1), quantity)
 print(get_numbers_ticket(600, 1000, 6))
